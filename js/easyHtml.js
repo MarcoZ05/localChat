6a3070025a2d5dbbe68265d3f430d2ebaa8720d7
Käkäsh <77887301+MarcoZ05@users.noreply.github.com>
2022-03-05 12:47:29 +0100

    format files

--- js/easyHtml.js
+++ js/easyHtml.js
@@ -1,19 +1,19 @@
 export function elemById(id) {
-    return document.getElementById(id)
+  return document.getElementById(id);
 }
 
-export function randInt(min,max) {
-    return Math.floor(Math.random()*(max-min+1))+min
+export function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export function cLog(msg) {
-    console.log(msg)
+  console.log(msg);
 }
 
 export function getStor(name) {
-    return localStorage.getItem(name)
+  return localStorage.getItem(name);
 }
 
-export function setStor(name,value) {
-    localStorage.setItem(name,value)
+export function setStor(name, value) {
+  localStorage.setItem(name, value);
 }
--- js/index.js
+++ js/index.js
@@ -1,75 +1,82 @@
-import * as htmlP from "/_export/easyHtml.js"
+import * as htmlP from "./easyHtml.js";
 
-const chatInput = htmlP.elemById("chat-input")
-const chatDiv = htmlP.elemById("chat-container")
-const chatButton = htmlP.elemById("chat-submit")
+const chatInput = htmlP.elemById("chat-input");
+const chatDiv = htmlP.elemById("chat-container");
+const chatButton = htmlP.elemById("chat-submit");
 
-const nameInput = htmlP.elemById("username-input")
-const nameButton = htmlP.elemById("username-submit")
-const nameDiv = htmlP.elemById("username-container")
-const nameDescr = htmlP.elemById("username-description")
-const nameOnline = htmlP.elemById("username-online")
+const nameInput = htmlP.elemById("username-input");
+const nameButton = htmlP.elemById("username-submit");
+const nameDiv = htmlP.elemById("username-container");
+const nameDescr = htmlP.elemById("username-description");
+const nameOnline = htmlP.elemById("username-online");
 
 let nameArr;
 let name;
 
 function getName() {
-    name = nameInput.value
-    if(nameInput.value != ""){
-        nameDiv.remove()
-        
-    }
+  name = nameInput.value;
+  if (nameInput.value != "") {
+    nameDiv.remove();
+  }
 }
 
 function sendMsg(msg) {
-    if(msg != ""){
-        chatInput.value = ""
-        chatDiv.innerHTML += "<span id='"+index+"'><b>" + name + ":</b>" +"<br>"+ msg + "<br><br>"
-        htmlP.setStor("chat",chatDiv.innerHTML)
-    }else{
-    }
+  if (msg != "") {
+    chatInput.value = "";
+    chatDiv.innerHTML +=
+      "<span id='" +
+      index +
+      "'><b>" +
+      name +
+      ":</b>" +
+      "<br>" +
+      msg +
+      "<br><br>";
+    htmlP.setStor("chat", chatDiv.innerHTML);
+  } else {
+  }
 }
 
 function loadMsg() {
-    let msg = htmlP.getStor("chat")
-    
-    chatDiv.innerHTML = msg;
+  let msg = htmlP.getStor("chat");
+
+  chatDiv.innerHTML = msg;
 }
 
 function startChat() {
-    if(htmlP.getStor("chat") != null){
-        loadMsg()
-    }else{
-        htmlP.getStor("chat","")
-    }
+  if (htmlP.getStor("chat") != null) {
+    loadMsg();
+  } else {
+    htmlP.getStor("chat", "");
+  }
 }
 
 function clickButton() {
-    sendMsg(chatInput.value)
+  sendMsg(chatInput.value);
 }
 
 function refreshOnlineNames() {
-    // lade wenn name eingegeben (getName), localstorage load true => jeder client schickt name ins array ein
+  // lade wenn name eingegeben (getName), localstorage load true => jeder client schickt name ins array ein
 }
 
 function resetChat() {
-    chatInput.value = ""
-    chatDiv.innerHTML += ""
-    htmlP.setStor("chat",chatDiv.innerHTML)
+  chatInput.value = "";
+  chatDiv.innerHTML += "";
+  htmlP.setStor("chat", chatDiv.innerHTML);
 }
 
 startChat();
-nameButton.addEventListener("click",getName)
-nameInput.addEventListener("keyup", function(event) {
-    if (event.keyCode == 13) {
-        getName()
-    }
+nameButton.addEventListener("click", getName);
+nameInput.addEventListener("keyup", function (event) {
+  if (event.keyCode == 13) {
+    getName();
+  }
 });
-chatButton.addEventListener("click",clickButton)
-chatInput.addEventListener("keyup", function(event) {
-    if (event.keyCode == 13) {
-        clickButton()
-    }
+chatButton.addEventListener("click", clickButton);
+chatInput.addEventListener("keyup", function (event) {
+  if (event.keyCode == 13) {
+    clickButton();
+  }
 });
-  
-window.onstorage = loadMsg
+
+window.onstorage = loadMsg;
